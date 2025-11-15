130e38b597ae0f87eae6a13f05c54b91c254437a
José Andres <contretaa@gmail.com>
2025-11-15 13:33:20 -0400

    new logs added
    Signed-off-by: José Andres <contretaa@gmail.com>

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,8 @@
 export const onRequest = ({ request, redirect, locals }, next) => {
-  const header = request.headers.get('accept-language') ?? 'en';
+  const header = request.headers.get('Accept-Language') ?? 'en';
   const countryCode = request.headers.get('cf-ipcountry');
 
-  console.log('hello world', countryCode);
+  console.log(header, countryCode, 'newwwww');
   const lang = header.split(',')[0].slice(0, 2);
   locals.lang = ['en', 'es'].includes(lang) ? lang : 'en';
   return next();
