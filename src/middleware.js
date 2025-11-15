a0bbf7a77fa582958ddd5bba5dce006d5f909593
José Andres <contretaa@gmail.com>
2025-11-15 13:30:12 -0400

    debug: print user countryCode
    Signed-off-by: José Andres <contretaa@gmail.com>

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,8 @@
 export const onRequest = ({ request, redirect, locals }, next) => {
   const header = request.headers.get('accept-language') ?? 'en';
+  const countryCode = request.headers.get('cf-ipcountry');
+
+  console.log('hello world', countryCode);
   const lang = header.split(',')[0].slice(0, 2);
   locals.lang = ['en', 'es'].includes(lang) ? lang : 'en';
   return next();
